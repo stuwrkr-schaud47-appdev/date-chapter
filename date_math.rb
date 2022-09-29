beb6692d62314772ba7c008dc96d27a5e3dd326c
stuwrkr-schaud47 <stuwrkr-schaud47@mx.uillinois.edu>
2022-09-29 19:46:42 +0000

    done

--- date_math.rb
+++ date_math.rb
@@ -1,7 +1,10 @@
 # Should output the number of days since Ruby was made.
-# 
+#
 # (Ruby released to the public on `December 21, 1995`.)
-# 
+#
 # Output:
 #   "Ruby is 108937 days old!"
 #
+require "date"
+number_of_days = Date.today - Date.parse("December 21, 1995")
+p number_of_days.to_i
--- date_monday.rb
+++ date_monday.rb
@@ -7,6 +7,11 @@
 #   "Is today Monday? false"
 #
 # if today is not Monday.
-require "date"
-bool = Date.today.monday?
-p "Is today Monday? " + bool.to_s
+# require "date"
+# bool = Date.today.monday?
+# p "Is today Monday? " + bool.to_s
+
+p "enter a word:"
+user_words = gets.chomp.split("")
+p "user_words:"
+p user_words
